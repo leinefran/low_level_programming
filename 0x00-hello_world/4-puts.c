70dddcb6aae3da6592653cc57d90bed0c1d9695b
vagrant <vagrant@homestead.test>
2018-06-20 22:54:32 +0000

    print msg using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-
-	getchar();
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
