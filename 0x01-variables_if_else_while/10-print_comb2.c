5e9b6dbe5c5bbf83e1b98187b168edeb493cde50
vagrant <vagrant@homestead.test>
2018-06-22 05:54:13 +0000

    added if

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints the numbers from 00 to 99
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0' ; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (j != '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
