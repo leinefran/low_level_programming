de05632f4d30455c2f5eeb955c70d1ae9934415e
leinefran <penny.leine@gmail.com>
2018-06-27 23:27:50 +0000

    finished version post pld

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,14 +7,42 @@
  */
 void times_table(void)
 {
-	int row, column;
+	int row, column, x, y;
 
-	for (row = '0' ; row < '9' ; row++)
+	for (row = 0 ; row <= 9 ; row++)
 	{
-		for (column = '0' ; column < '9' ; column++)
+		for (column = 0 ; column <= 9 ; column++)
 		{
-			_putchar(row);
-			_putchar(column);
+			x = (row * column) / 10;
+			y = (row * column) % 10;
+
+			if ((row * column) <= 9)
+			{
+				if (column != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(y + '0');
+				if (column != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if (x != 0)
+			{
+				_putchar(x + '0');
+				_putchar(y + '0');
+				if ((row * column) != 81)
+				{
+					if (column != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
 		}
+		_putchar('\n');
 	}
 }
