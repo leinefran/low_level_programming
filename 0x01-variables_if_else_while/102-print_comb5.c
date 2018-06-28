f90e0cbcc78d272cd8460562f0aa3372838534ae
leinefran <penny.leine@gmail.com>
2018-06-28 00:45:02 +0000

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,24 @@ int main(void)
 {
 	int a, b, i, j, k, l;
 
-	for (a = '0'; a <= '98' ; a++)
+	for (a = 0 ; a <= 98 ; a++)
 	{
-		for (b = '0' ; b <= '99'; b++)
+		for ( b = a + 1 ; b <= 99 ; b++)
 		{
 			i = a / 10;
 			j = a % 10;
 			k = b / 10;
 			l = b % 10;
-			putchar(i + 0);
-			putchar(j + 0);
+			putchar(i + '0');
+			putchar(j + '0');
 			putchar(' ');
-			putchar(k + 0);
-			putchar(l + 0);
-		}
-		if (a != 98 && b != '99')
-		{
+			putchar(k + '0');
+			putchar(l + '0');
+			if (a != 98 || b != 99)
+			{
 			putchar(',');
 			putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
