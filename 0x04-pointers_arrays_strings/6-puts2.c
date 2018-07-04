13158d6f0ea06f9e0c32bf6593c1d42279e0fe8a
leinefran <penny.leine@gmail.com>
2018-07-04 01:57:14 +0000

    prints one char out of 2 of a string, followed by a new line

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string, followed by a new line
+ * @str: points to the address of the 1st character of the string to be printed
+ *
+ * Return: Nothing.
+ */
+void puts2(char *str)
+{
+	int number;
+	number = 0;
+
+	while (str[number] != '\0')
+	{
+		if (number % 2 == 0)
+		_putchar(str[number]);
+		number++;
+	}
+	_putchar('\n');
+}
