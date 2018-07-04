ceb897a87a01a1f53a5647419e4a64554b8b0155
leinefran <penny.leine@gmail.com>
2018-07-04 04:27:52 +0000

    prints n elements of an array of integers + \n

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -17,11 +17,11 @@ void print_array(int *a, int n)
 
 	count = 0;
 
-	while (count < n && count != (n - 1))
+	while (count < (n - 1))
 	{
 		printf("%d, ", (a[count]));
 		count++;
-		if (count >= (n - 1))
+		if (count == (n - 1))
 			printf("%d\n", (a[count]));
 	}
 }
