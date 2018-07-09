362e24eb0c1181af3032ad97f00d9958fdc22c2f
leinefran <penny.leine@gmail.com>
2018-07-09 02:08:41 +0000

    switched to a different solution

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -9,19 +9,16 @@
  */
 char *rot13(char *s)
 {
-	int i;
+	char a1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char a2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	int i, j;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-		for (i = 0 ; (s[i] >= 'a' && s[i] <= 'm') ||
-			     (s[i] >= 'A' && s[i] <= 'M') ||
-			     (s[i] != '\0'); i++)
-			s[i] = (s[i] + 13);
-
-		if ((s[i] >= 'n' && s[i] <= 'z') ||
-		    (s[i] >= 'N' && s[i] <= 'Z'))
-			s[i] = (s[i] - 13);
-		i++;
+		for (j = 0 ; j <= 51 ; j++)
+			if (s[i] == a1[j])
+			    s[i] = a2 [j];
 	}
 	return (s);
 }
