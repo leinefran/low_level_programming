4d2891066d60582e6846364f6e7a2d62a06646b2
leinefran <penny.leine@gmail.com>
2018-07-10 21:14:32 +0000

    passed betty test and correct oup

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ *
+ * @s: pointer to 1st char of string to be reverse.
+ * @n: number of elements.
+ * @b: constant byte.
+ *
+ * Return: char.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
