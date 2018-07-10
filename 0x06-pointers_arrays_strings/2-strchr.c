2cd810cc25386ed0839d8603cde0919b6fc89ba6
leinefran <penny.leine@gmail.com>
2018-07-10 22:26:21 +0000

    passed betty, correct output

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strchr - a function that locates a character in a string.
+ *
+ * @s: the string.
+ * @c: the character the function is looking for.
+ *
+ * Return: char.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	if (c == '\0')
+		return (s);
+	return (0);
+}
