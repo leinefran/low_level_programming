2b33d7aae9dd569704680adef6a3aa3eba71086a
leinefran <penny.leine@gmail.com>
2018-07-11 02:15:05 +0000

    a function that searches a string for any of a set of bytes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes.
+ * _putchar -  prints a char at a time.
+ *
+ * @s: the string.
+ * @accept: the initial segment of s.
+ *
+ * Return: char.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0 ; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s = s + i;
+				break;
+			}
+		}
+			if (s[i] != accept[j])
+				return (0);
+	}
+			return (s);
+}
