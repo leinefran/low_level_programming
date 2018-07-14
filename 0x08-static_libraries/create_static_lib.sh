29473e83e94747632917f3e626c27cddce92a18f
leinefran <penny.leine@gmail.com>
2018-07-14 18:36:45 +0000

    added gcc flags

--- 0x08-static_libraries/create_static_lib.sh
+++ 0x08-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ar rc liball.a 0-isupper.o 1-isdigit.o 2-strlen.o 3-strcmp.o 5-strstr.o 0-memset.o 1-memcpy.o  2-strncpy.o 3-strspn.o  6-abs.o 0-strcat.o 1-strncat.o 3-islower.o 4-isalpha.o 9-strcpy.o 100-atoi.o 2-strchr.o 3-puts.o 4-strpbrk.o _putchar.o
-
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar rc liball.a *.o
