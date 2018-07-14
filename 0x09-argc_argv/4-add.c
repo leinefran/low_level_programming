52f7966b1ea7a14faf002e8ac54010cb4122cbfc
leinefran <penny.leine@gmail.com>
2018-07-14 20:27:11 +0000

    Fixed: body of function

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,6 +1,6 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - a global function designated start of the program.
@@ -10,31 +10,31 @@
  *
  * Return: (0).
  */
-int isdigit(int c);
-int isalpha(int argument);
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
-	if (argc != 3)
-		printf("0\n");
-
-	for (i = 1 ; i <= 9 ; i++)
-	{
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-	return (0);
-
-	for (i = 1; i < argc ; i++)
-	{
-		if(isalpha(argv[i])))
+	if (argc == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	for (i = 1 ; i < argc ; i++)
+	{
+		for (j = 0 ; argv[i][j] != '\0' ; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
