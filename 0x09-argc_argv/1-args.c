66630af25942f8ded1a7ea5a27f4667985de8c8c
leinefran <penny.leine@gmail.com>
2018-07-14 01:10:15 +0000

    prints the number of arguments passed into it

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - a global function designated start of the program.
+ *
+ * @argc: contains the numbers of arguments passed to the program.
+ * @argv: array of strings.
+ *
+ * Return: int.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
