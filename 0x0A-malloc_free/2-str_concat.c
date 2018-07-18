d881942346a65a7c55516b1dd0144663c37a238d
leinefran <penny.leine@gmail.com>
2018-07-18 01:26:16 +0000

    Changed first if statement

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -28,8 +28,11 @@ char *str_concat(char *s1, char *s2)
 
 	n = s1_len + s2_len + 1;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1[0] = '\0';
+	if (s2 == NULL)
+		s2[0] = '\0';
+
 
 	ls = malloc(sizeof(char) * n);
 
