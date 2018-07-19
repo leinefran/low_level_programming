71fc7aa38cfd1ff91a32786aa06f7ea2003dd137
leinefran <penny.leine@gmail.com>
2018-07-19 19:36:46 +0000

    Fixed: pointer variable type

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -7,17 +7,16 @@
  * If malloc fails, the malloc_checked function should cause normal process
  * termination with a status value of 98
  *
- * @size: size of the array.
- * @c: every char in array;
+ * @b: size.
  *
  * Return: pointer to allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	char *p;
 
 	p = malloc(b);
 	if (p == NULL)
-		exit (98);
+		exit(98);
 	return (p);
 }
