288940be8924e42dcc9a035a3b781b9210784fd0
leinefran <penny.leine@gmail.com>
2018-07-20 03:38:42 +0000

    created a new variable

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -12,17 +12,22 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i;
+	int i, range;
+
+	range = (max - min + 1);
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * (max - min + 1));
+	ptr = malloc(sizeof(int) * range);
 
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	for (i = 0; i < (max + 1) ; i++)
+	for (i = 0; i < range ; i++)
 	{
 		ptr[i] = min;
 		min++;
