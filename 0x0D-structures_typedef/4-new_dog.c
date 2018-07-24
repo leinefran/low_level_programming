2aaaf0300794bcde487cd309164a0ba0c1392fe2
leinefran <penny.leine@gmail.com>
2018-07-24 02:50:25 +0000

    Added:  dest[counter] = '\0' at the end of _strcpy

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -18,7 +18,7 @@ char *_strcpy(char *dest, char *src)
 
 		for (counter = 0; src[counter] != '\0'; counter++)
 			dest[counter] = src[counter];
-		dest[counter] = '\n';
+		dest[counter] = '\0';
 
 		return (dest);
 	}
