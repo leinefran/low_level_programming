bc1d9a90e6d73c791d79ece69a8f8e95716851cb
leinefran <penny.leine@gmail.com>
2018-07-25 22:31:36 +0000

    contains only one function

---
+++ 0x0E-function_pointers/op_functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "calc.h"
+
+/**
+ * op_add - add a and b.
+ * @a: parameter.
+ * @b: parameter.
+ *
+ * Return: the sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
