e7628454456db24e5e69462bf9d49cd35f8c91a9
leinefran <penny.leine@gmail.com>
2018-07-25 22:53:06 +0000

    contains all the 5 functions

--- 0x0E-function_pointers/op_functions.c
+++ 0x0E-function_pointers/op_functions.c
@@ -12,3 +12,51 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op-sub - subtract a and b.
+ * @a: parameter.
+ * @b: parameter.
+ *
+ * Return: the difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiples a and b.
+ * @a: parameter.
+ * @b: parameter.
+ *
+ * Return: the product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide a and b.
+ * @a: parameter.
+ * @b: parameter.
+ *
+ * Return: the result of the division of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b.
+ * @a: parameter.
+ * @b: parameter.
+ *
+ * Return: the remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a / b % 10);
+}
