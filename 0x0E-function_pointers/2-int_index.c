f2f416a4c9fd0706c20a3be14edb1ff7593a955d
leinefran <penny.leine@gmail.com>
2018-07-25 21:18:36 +0000

    Fixed: betty

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -19,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		for (i= 0; i < size ; i++)
+		for (i = 0; i < size ; i++)
 		{
 			if (cmp(array[i]))
 				return (i);
