751db0b41e6e18dd40ce3c784dd0ac563d38e7aa
leinefran <penny.leine@gmail.com>
2018-08-03 21:41:30 +0000

    Fixed: betty style

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -4,13 +4,13 @@
  * is_prime_number - verifies if an interger is a prime number.
  *
  * @n: the number to be verified.
- *
+ * @div: the divisor.
  * Return: 1 if the input interger is a prime number; 0 otherwise.
  */
 int is_prime_helper(int n, int div);
 int is_prime_number(int n)
 {
- 	if (n < 2)
+	if (n < 2)
 		return (0);
 	return (is_prime_helper(n, 2));
 
