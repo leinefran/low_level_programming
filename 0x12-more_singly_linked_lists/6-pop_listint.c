b3b2d2dac5e471c2f547dd04cc7e228df21be9fb
leinefran <penny.leine@gmail.com>
2018-08-07 18:14:43 +0000

    Changed: data value from tmp->n to (*head)->n

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -12,11 +12,11 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int data;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (0);
 
 	tmp = (*head)->next;
-	data = tmp->n;
+	data = (*head)->n;
 	free(*head);
 	*head = tmp;
 
