c81a34525c7c3a57230492ca429edcbb855e2971
leinefran <penny.leine@gmail.com>
2018-08-07 22:34:45 +0000

    Added: if statement

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+
 	while (count < idx)
 	{
 
