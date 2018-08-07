fe226f22764d7a46d7221b02f07d21c81c0603c2
leinefran <penny.leine@gmail.com>
2018-08-07 22:41:58 +0000

    Added: if statement inside while loop checking if current is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,8 +35,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (count < idx)
 	{
-
 		current = current->next;
+		if (current == NULL)
+			return (NULL);
 		count++;
 	}
 	new_node->next = current->next;
