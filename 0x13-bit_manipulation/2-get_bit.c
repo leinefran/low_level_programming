988d004438fd299323366142cf7153e5b4eea4fb
leinefran <penny.leine@gmail.com>
2018-08-10 03:49:23 +0000

    a function that returns the value of a bit at a given index

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * get_bit - a function that returns the value of a bit at
+ * a given index.
+ * @n: points to the number being passed to the function.
+ * @index: the index starting at 0.
+ * Return: the value of the bit at index index;
+ * or -1 if an error occured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int shift;
+
+	if (index == 0)
+		return (0);
+
+	else
+	{
+		while (index > 0)
+		{
+			shift = n >> index;
+			if (shift & 1)
+				return (1);
+		}
+	}
+	return (0);
+}
