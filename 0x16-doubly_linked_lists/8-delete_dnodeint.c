b75dd89f97122afcf2aef4b225b1d4e11ef49919
leinefran <penny.leine@gmail.com>
2018-09-17 02:46:13 +0000

    Added: if statement to check if *head exists

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		if ((*head)->next)
+		if (*head)
 			(*head)->prev = NULL;
 		free(current);
 		return (1);
