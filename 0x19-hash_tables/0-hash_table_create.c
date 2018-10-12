341ca08d1f07628a5f486cfd91aed9ff5a9d7b9f
leinefran <penny.leine@gmail.com>
2018-10-12 01:56:34 +0000

    defined size

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -8,17 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_node_t **array;
 
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *));
+	table->array = malloc(sizeof(hash_node_t) * size);
 
-	if (array == NULL)
+	if (table->array == NULL)
 		return (NULL);
+
+	table->size = size;
 
 	return (table);
 }
