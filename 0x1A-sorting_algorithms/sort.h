9cdb0b950e8c8f5689d9b4b8c4f60100629c5b75
leinefran <penny.leine@gmail.com>
2018-10-26 22:23:14 +0000

    Added: swap function prototype

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -1,6 +1,9 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,6 +18,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void swap_int(int *a, int *b);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
