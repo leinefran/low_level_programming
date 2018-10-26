a87db15f4037fecceda79aa579d0b061bf6961fe
leinefran <penny.leine@gmail.com>
2018-10-26 22:24:09 +0000

    a function that swaps two integers

---
+++ 0x1A-sorting_algorithms/swap.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: is the pointer to be swaped with b
+ * @b: is the pointer to be swaped with a
+ *
+ * Return: Always 0.
+ */
+void swap(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+
+	*a = *b;
+	*b = x;
+}
