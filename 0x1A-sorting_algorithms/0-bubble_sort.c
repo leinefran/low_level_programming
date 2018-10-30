65ddc219038c1d61dd8386aa083994518f5ca805
leinefran <penny.leine@gmail.com>
2018-10-30 22:22:45 +0000

    Update

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -11,6 +11,9 @@ void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size-1; i++)
 	{
 		/* Last i elements are already in place */
