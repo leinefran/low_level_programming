73c043942d4b567e10b473ca154cc50bcdc12e5b
leinefran <penny.leine@gmail.com>
2018-10-31 21:12:04 +0000

    a function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+void qs_helper(int *array, size_t size, int low, int high);
+int partition(int *array, size_t size, int low, int high);
+/**
+ * quicksort - a function that sorts an array of integers
+ * in ascending order using the quicksort algorith & lomuto partition.
+ * @array: the array to be sorted.
+ * @size: the size of the array.
+ * Return: Nothing.
+ */
+void quick_sort(int *array, size_t size)
+{
+	qs_helper(array, size, 0, size - 1);
+}
+/**
+ * qs_helper - sorts elements low through high.
+ * @array: the array to be sorted.
+ * @size: size of array.
+ * @low: lowest elements.
+ * @high: highest elements.
+ * Return: Nothing.
+ */
+void qs_helper(int *array, size_t size, int low, int high)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, size, low, high);
+		qs_helper(array, size, low, pivot - 1);
+		qs_helper(array, size, pivot + 1, high);
+	}
+}
+/**
+ * partition - a function that takes last element as pivot,
+ * places the pivot element at its correct position in sorted array,
+ * and places all smaller than pivot to left of pivot & all greater elements
+ * to right of pivot.
+ * @array: the array to be sorted.
+ * @size: size of array.
+ * @low: lowest elements.
+ * @high: highest elements.
+ */
+int partition(int *array, size_t size, int low, int high)
+{
+	int pivot = array[high];
+	int i = (low - 1), j, tmp;
+
+	for (j = low ; j <= high - 1 ; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+		}
+	}
+	tmp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = tmp;
+	print_array(array, size);
+	return (i + 1);
+}
