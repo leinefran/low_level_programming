424aa9bb2d30cc282b06ec2ea91d0d5d45beea27
leinefran <penny.leine@gmail.com>
2018-10-31 17:01:53 +0000

    Updated: body of function

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -7,27 +7,25 @@
  * @size: the size of the array.
  * Return: Nothing.
  */
-void swap(int *a, int *b);
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx, tmp;
 
-	min_idx = i;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
-				tmp = array[j];
-				array[j] = array[min_idx];
-				array[min_idx] = tmp;
+				min_idx = j;
 			}
-			print_array(array, size);
 		}
 		if (min_idx != i)
 		{
-			swap(&array[min_idx], &array[i]);
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
 			print_array(array, size);
 		}
 	}
