21b6d8313a9e77e03c011c89ca924c30a0fcd11d
leinefran <penny.leine@gmail.com>
2018-12-13 02:27:22 +0000

    first draft

---
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree.
+ * @tree: a pointer to the root node.
+ * Return: if tree is NULL return 0.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+
+}
