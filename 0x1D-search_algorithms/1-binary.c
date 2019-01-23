2f0e5ec4450bc0dbb17bcc564de80c8337421c18
leinefran <penny.leine@gmail.com>
2019-01-23 21:24:05 +0000

    Firt version of a function that searches for a value in a sorted array of integers using the Binary search algorithm. Still working on printing the array being searched every time it changes.

---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - a function that searches for a value in an array of integers
+ * using the Binary search algorithm;
+ * @array: a pointer to the 1st element;
+ * @size: the number of elements in the array;
+ * @value: the value to search for
+ * Return: the first index where value is located.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+	int middle = 0;
+
+	if (!array)
+		return (-1);
+
+	while (left <= right)
+	{
+		/* for (left = 0; array[left] != '\0'; left++)
+		{
+			printf("Searching in array: %i", array[left]);
+			}*/
+
+		middle = (left + right) / 2;
+
+		if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else if (array[middle] > value)
+		{
+			right = middle - 1;
+		}
+		else
+			return (middle);
+	}
+	return (-1);
+}
